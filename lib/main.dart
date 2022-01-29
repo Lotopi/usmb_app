bb2f55e2d28254ac5b54383ebbe60d3bb8417f4e
Lotopi <40468597+Lotopi@users.noreply.github.com>
2022-01-29 23:20:08 +0100

    Adds all the features of the calendar (changes the package used)

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,141 @@
 import 'package:flutter/material.dart';
 
-import 'package:calendar_view_multi_language/calendar_view.dart';
+import 'package:flutter_week_view/flutter_week_view.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/services.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'dart:convert';
 
-  // This widget is the root of your application.
+void main() => runApp(_MyApp());
+
+/// The demo material app.
+class _MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return CalendarControllerProvider(
-      controller: EventController(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Emploi du temps',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => inScaffold(body: _DynamicWeekView())
+        },
+      );
+
+  static Widget inScaffold({
+    String title = 'Emploi du temps',
+    required Widget body,
+  }) =>
+      Scaffold(
+        appBar: AppBar(
+          title: Text(title),
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      )
-    );
-  }
+        body: body,
+      );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+/// A day view that displays dynamically added events.
+class _DynamicWeekView extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _DynamicWeekViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+/// The dynamic day view state.
+class _DynamicWeekViewState extends State<_DynamicWeekView> {
+
+  List _items = [];
+  List<FlutterWeekViewEvent> events = [];
+
+  Future<void> _getEvents() async {
+    /*
+      This is an asynchronous function used to load the events from a JSON file.
+    */
+
+    final String response = await rootBundle.loadString('assets/calendar.JSON');
+    final data = await json.decode(response);
+
+    setState(() {
+      _items = data["classes"]["L2-S3-INFO-1"];
+    });
+    
+    for (var i = 0; i < _items.length; i++) {
+      final DateTime startDate = DateTime.parse(_items[i]['DTSTART']);
+      final DateTime endDate = DateTime.parse(_items[i]['DTEND']);
+
+      final event = 
+        FlutterWeekViewEvent(
+          title: '${_items[i]['SUMMARY']}',
+          description: '${_items[i]['LOCATION']} \n${_items[i]['DESCRIPTION']}',
+          start: startDate,
+          end: endDate,
+          margin: const EdgeInsets.all(4.0),
+          decoration: BoxDecoration(
+            color: const Color(0xFF2196F3),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        );
+
+      setState(() {
+        events.add(event);
+      });
+    }
+  }
+
+  // A boolean that will prevent multiple loading of the same data.
+  var eventsAreLoaded = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      body: WeekView(),
+    DateTime now = DateTime.now();
+
+    // Load the events if needed.
+    if (!eventsAreLoaded) {
+      _getEvents();
+      eventsAreLoaded = true;
+    }
+
+    List<DateTime> getDaysInBeteween(DateTime startDate, DateTime endDate) {
+      /*
+        Return the list of all dates between two given dates.
+
+        Parameters:
+          - startDate: The start date.
+          - endDate: The end date.
+      */
+
+      List<DateTime> days = [];
+      for (int i = 0; i <= endDate.difference(startDate).inDays; i++) {
+        days.add(
+          DateTime(
+            startDate.year, 
+            startDate.month,
+            startDate.day + i)
+        );
+      }
+        return days;
+    }
+
+    // The list of dates that the calendar must display.
+    List<DateTime> dates = [];
+
+    // Get the dates for the current school year.
+    if (now.month < 07) {
+      dates = getDaysInBeteween(DateTime(now.year - 1, 08, 30), DateTime(now.year, 07, 01));
+    } else {
+      dates = getDaysInBeteween(DateTime(now.year, 08, 01), DateTime(now.year + 1, 07, 01)); 
+    }
+
+    // This is the function used to customize the style of a day.
+    DayViewStyle funcTest(DateTime date) => const DayViewStyle(hourRowHeight: 60 * 2);
+
+    return WeekView(
+      initialTime: const HourMinute(hour: 6, minute: 31).atDate(DateTime.now()),
+      minimumTime: const HourMinute(hour: 6, minute: 30),
+      maximumTime: const HourMinute(hour: 21),
+      hoursColumnStyle: const HoursColumnStyle(
+        interval: Duration(minutes: 30),
+      ),
+      userZoomable: false,
+      dates: dates,
+      dayViewStyleBuilder: funcTest,
+      events: events,
     );
   }
 }
