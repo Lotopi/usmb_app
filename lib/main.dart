940c3c0eb90970cbb32d0747d4bd33a7bf76e623
Lotopi <40468597+Lotopi@users.noreply.github.com>
2022-02-02 21:38:59 +0100

    Add a drop-down list to change the class

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,122 @@ import 'package:flutter/services.dart';
 
 import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() => runApp(_MyApp());
 
-/// The demo material app.
-class _MyApp extends StatelessWidget {
+
+class _MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+/// The demo material app.
+class _MyAppState extends State<_MyApp> {
+
+  final List<String> listItem = ["Aucune"];
+  String valueChoose = "Aucune";
+
+  String selectedClass = "Aucune";
+
+  var _dropDownItems;
+
+  Future<void> _getGroups() async {
+    /*
+      This is an asynchronous function used to load the groups from a JSON file.
+    */
+
+    final String response = await rootBundle.loadString('assets/calendar.JSON');
+    final data = await json.decode(response);
+
+    setState(() {
+      _dropDownItems = data["classes"];
+    });
+    
+    for (var i = 0; i < _dropDownItems.length; i++) {
+      String key = _dropDownItems.keys.elementAt(i);
+
+      setState(() {
+        listItem.add(key);
+      });
+    }
+  }
+
+  Future<void> _getStringFromSharedPref() async {
+    /*
+      This asynchronous function get the value of 
+      the selected class.
+    */
+    final prefs = await SharedPreferences.getInstance();
+    final storedSelectedClass = prefs.getString('selectedClass');
+
+    setState(() {
+      selectedClass = storedSelectedClass ?? "Aucune";
+      valueChoose = selectedClass;
+    });
+  }
+
+  // A boolean that will prevent multiple loading of the same data.
+  var dropDownItemmsAreLoaded = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Load the events if needed.
+    if (!dropDownItemmsAreLoaded) {
+      _getGroups().then((_) => _getStringFromSharedPref());
+      dropDownItemmsAreLoaded = true;
+    }
+
+    return MaterialApp(
         title: 'Emploi du temps',
         initialRoute: '/',
         routes: {
           '/': (context) => inScaffold(body: _DynamicWeekView())
         },
       );
+  }
 
-  static Widget inScaffold({
+  Future<void> _changeSelectedClass(newSelectedClass) async {
+    /*
+      This asynchronous function store the newly selected value in
+      shared preferences.
+    */
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('selectedClass', newSelectedClass);
+  }
+
+  Widget inScaffold({
     String title = 'Emploi du temps',
     required Widget body,
   }) =>
       Scaffold(
         appBar: AppBar(
+          actions: [
+            DropdownButton<String>(
+              value: valueChoose,
+              onChanged: (newValue) {
+                setState(() {
+                  valueChoose = newValue as String;
+                  //selectedClass = valueChoose;
+                  //_changeSelectedClass(valueChoose);
+                });
+
+                _changeSelectedClass(valueChoose);
+              },
+              items: listItem.map((valueItem) {
+                return DropdownMenuItem(
+                  value: valueItem,
+                  child: SizedBox(
+                    child: Text(valueItem),
+                    width: 100,
+                  ),//Text(valueItem),
+                );
+              }).toList(),
+              )
+          ],
           title: Text(title),
         ),
         body: body,
@@ -40,19 +137,57 @@ class _DynamicWeekView extends StatefulWidget {
 /// The dynamic week view state.
 class _DynamicWeekViewState extends State<_DynamicWeekView> {
 
+  String selectedClass = "";
+
+  Future<void> _getStringFromSharedPref() async {
+    /*
+      This asynchronous function get the value of 
+      the selected class. 
+    */
+
+    final prefs = await SharedPreferences.getInstance();
+    final storedSelectedClass = prefs.getString('selectedClass');
+
+    setState(() {
+      selectedClass = storedSelectedClass ?? "Aucune";
+    });
+  }
+
+  Future<void> _getActualStringFromSharedPref() async {
+    /*
+      This asynchronous function get the actual value of 
+      the selected class. 
+      { Used for comparison }
+    */
+
+    final prefs = await SharedPreferences.getInstance();
+    final storedSelectedClass = prefs.getString('selectedClass');
+
+    setState(() {
+      actualSelectedClass = storedSelectedClass ?? "Aucune";
+    });
+  }
+
   List _items = [];
   List<FlutterWeekViewEvent> events = [];
 
   Future<void> _getEvents() async {
     /*
-      This is an asynchronous function used to load the events from a JSON file.
+      This asynchronous function load the events from a JSON file.
     */
 
     final String response = await rootBundle.loadString('assets/calendar.JSON');
     final data = await json.decode(response);
 
     setState(() {
-      _items = data["classes"]["L2-S3-INFO-1"];
+      /* Try to load the selected class, if something goes wrong
+         it will fail back onto the default class. */
+      try {
+        _items = data["classes"][selectedClass];
+      }
+      catch(e) {
+        _items = data["classes"]["Aucune"];
+      }
     });
     
     for (var i = 0; i < _items.length; i++) {
@@ -62,7 +197,7 @@ class _DynamicWeekViewState extends State<_DynamicWeekView> {
       final event = 
         FlutterWeekViewEvent(
           title: '${_items[i]['SUMMARY']}',
-          description: '${_items[i]['LOCATION']} \n${_items[i]['DESCRIPTION']}',
+          description: '${_items[i]['LOCATION']} \n\n${_items[i]['DESCRIPTION']}',
           start: startDate,
           end: endDate,
           margin: const EdgeInsets.all(4.0),
@@ -78,8 +213,17 @@ class _DynamicWeekViewState extends State<_DynamicWeekView> {
     }
   }
 
+  Future<void> _reloadEvents() async {
+    setState(() {
+      events = [];
+      _getEvents();
+    });
+  }
+
   // A boolean that will prevent multiple loading of the same data.
   var eventsAreLoaded = false;
+
+  var actualSelectedClass = "Aucune";
 
   @override
   Widget build(BuildContext context) {
@@ -87,9 +231,23 @@ class _DynamicWeekViewState extends State<_DynamicWeekView> {
 
     // Load the events if needed.
     if (!eventsAreLoaded) {
-      _getEvents();
+      _getStringFromSharedPref().then((_) {
+        print(selectedClass);
+        _getEvents();
+      });
+      
       eventsAreLoaded = true;
     }
+
+    // Check if the selected class was changed, if true, will reload the events.
+    _getActualStringFromSharedPref().then((value) => 
+      {
+        if (selectedClass != actualSelectedClass) {
+          _reloadEvents()
+        }
+      }
+    );
+    
 
     List<DateTime> getDaysInBeteween(DateTime startDate, DateTime endDate) {
       /*
